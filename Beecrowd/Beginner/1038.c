0d26cbf025d2e61b7242b80924013c270b7336c3
MD.Kawsar Hasan <kawsarsf21@gmail.com>
2026-07-04 06:30:28 +0600

    Add files via upload

---
+++ Beecrowd/Beginner/1038.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int X,Y;
+ scanf("%d %d",&X,&Y);
+ (X==1)?
+ printf("Total: R$ %.2f\n",Y*4.00):
+(X==2)?
+ printf("Total: R$ %.2f\n",Y*4.50):
+(X==3)?
+printf("Total: R$ %.2f\n",Y*5.00):
+(X==4)?
+printf("Total: R$ %.2f\n",Y*2.00):
+printf("Total: R$ %.2f\n",Y*1.50);
+return 0;
+}
