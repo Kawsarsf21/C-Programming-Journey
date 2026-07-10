f6cf1e7430d5a1ae1c53a5133a520118051789b9
MD.Kawsar Hasan <kawsarsf21@gmail.com>
2026-07-10 21:32:29 +0600

    Add files via upload

---
+++ Beecrowd/Beginner/1190.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int count=0;
+    double sum=0,M[12][12];
+    char O;
+    scanf("%c",&O);
+    for(int i=0;i<12;i++)
+    {
+        for(int j=0;j<12;j++)
+        {
+            scanf("%lf",&M[i][j]);
+            if(i<j&&i+j>11){sum+=M[i][j];count++;}
+        }
+    }
+    if(O=='S')printf("%.1lf\n",sum);
+    else printf("%.1lf\n",sum/count);
+    return 0;
+}
